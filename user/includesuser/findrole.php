9cb71721b809c810847e8d170143c67799d56adf
milastimec <taito.hira@gmail.com>
2021-06-07 13:50:38 +0200

    Update findrole.php

--- user/includesuser/findrole.php
+++ user/includesuser/findrole.php
@@ -1,7 +1,7 @@
 <?php
 
  function findrole($user){
-    require '../../includes/db_handler.inc.php';
+    require 'db_handler.inc.php';
     $sql= "select podjetjeID, naziv from dela_kot where osebaID = '".$user."';";
 
     $stmt = mysqli_stmt_init($conn);
@@ -18,10 +18,10 @@
  }
 
  function display($user, $company, $role){
-    require '../../includes/db_handler.inc.php';
+    require 'db_handler.inc.php';
 
     if($role == 'vodja'){
-        $sql = 'select * from dela_kot where podjetjeID = '.$company.';';
+        $sql = 'select * from dela_kot where podjetjeID = "'.$company.'";';
 
         $stmt = mysqli_stmt_init($conn);
         mysqli_stmt_prepare($stmt, $sql) or die(mysqli_error());
@@ -39,7 +39,7 @@
  }
 
  function getName($user){
-    require '/db_handler.inc.php';
+    require 'db_handler.inc.php';
 
     $sql= 'select ime, priimek from oseba where osebaID = "'.$user.'";';
 
